4c41d1144b7a3e964b4bd5fca74f1ae37a4f9236
Derszi65g <145814859+Derszi65g@users.noreply.github.com>
2025-10-16 11:24:03 -0500

    Add files via upload

---
+++ install-void-personal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo pacman -S i3 polybar rofi feh pywal maim xclip xdotool pcmanfm ttf-bitstream-vera emptty brightnessctl kitty ark emptty xorg
+
+[ -d ".config" ] && cp -r .config/* ~/.config/ && echo "Archivos de .config copiados a ~/.config." || echo "No se encontró .config."
+
+[ -d "wall" ] && cp -r wall ~/ && echo "Carpeta wall copiada a ~." || echo "No se encontró wall."S2
+
+[ -f ".config/polybar/launch.sh" ] && chmod +x ~/.config/polybar/launch.sh && echo "launch.sh ahora es ejecutable" || echo "No se encontró launch.sh."
+
+wal -i ~/wall/zd.png > /dev/null 2>&1 && echo -e "\x1B[1;5mPUEDES CAMBIAR EL WALLPAPER CON SUPER +X" || echo "fallo xd"
+
+sudo ln -s /etc/sv/emptty /var/service
